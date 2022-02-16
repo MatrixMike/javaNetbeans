ec766f0338f867703e2b4a12463339a8a4444180
MatrixMike <matroxmike@icloud.com>
2022-02-16 13:52:43 +1100

    from Netbeans 8.0.2 Latitude 3D java FX

---
+++ src/dateexamplemike1/DateExampleMike1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dateexamplemike1;
+
+/**
+ *
+ * @author mikeh
+ *
+ */
+public class DateExampleMike1 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hi there Mike\n");
+        System.out.println("hi there Mike\n");
+    }
+
+}
+/*  class void test1(int i){
+        }
+  
+  */
+    
